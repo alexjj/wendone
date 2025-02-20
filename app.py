dca9e7856c0a7774434f38c21a7d968e21d5af2d
Alex Johnstone <alexjj@gmail.com>
2025-02-20 08:29:37 +0000

    page settings

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import datetime
 target_datetime = datetime.datetime(2025, 2, 20, 13, 00, 0)
 current_datetime = datetime.datetime.now()
 
+st.set_page_config(layout='centered', page_title="wen done", page_icon=":older_man:")
+
 if current_datetime < target_datetime:
     st.image("notdone.png", caption="Not yet!")
 else:
