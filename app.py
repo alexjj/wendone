a56247c0383537f12f402d705f31da84f4211c66
Alex Johnstone <alexjj@gmail.com>
2025-02-20 08:24:57 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import datetime
+
+target_datetime = datetime.datetime(2025, 2, 11, 13, 00, 0)
+current_datetime = datetime.datetime.now()
+
+if current_datetime < target_datetime:
+    st.image("notdone.png", caption="Not yet!")
+else:
+    st.image("done.png", caption="It's time!")
+    st.balloons()
